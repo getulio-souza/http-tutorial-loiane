dc4195bd7e27e548a6bb3a03941fd55355dde761
Getulio Souza <getuliosouza8@gmail.com>
2024-12-03 16:21:30 -0300

    feature: http-tutorial-course

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CursosListaComponent } from './cursos/cursos-lista/cursos-lista.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: CursosListaComponent}
+];
---
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos-lista',
+  standalone: true,
+  imports: [],
+  templateUrl: './cursos-lista.component.html',
+  styles: ``
+})
+export class CursosListaComponent {
+
+}
